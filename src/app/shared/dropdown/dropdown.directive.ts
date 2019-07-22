ba5dac83b5865eb4a29e391e70a764d167a6fab9
jsavell <jsavell@library.tamu.edu>
2019-07-22 09:58:52 -0500

    fix dropdown directive formatting

--- src/app/shared/dropdown/dropdown.directive.ts
+++ src/app/shared/dropdown/dropdown.directive.ts
@@ -5,17 +5,19 @@ import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core'
 })
 
 export class DropdownDirective {
-  constructor(private _el: ElementRef) {
-  }
-  @HostBinding('class.show') isOpen = false;
-  @HostListener('click') toggleOpen(){
-    this.isOpen=!this.isOpen;
-    if(this.isOpen){
-      this._el.nativeElement.querySelector('.dropdown-menu').classList.add('show');
+    constructor(private _el: ElementRef) {
     }
-    else{
-      this._el.nativeElement.querySelector('.dropdown-menu').classList.remove('show');
-    }
-  }
 
+    @HostBinding('class.show')
+    isOpen = false;
+
+    @HostListener('click')
+    toggleOpen() {
+        this.isOpen=!this.isOpen;
+        if(this.isOpen) {
+            this._el.nativeElement.querySelector('.dropdown-menu').classList.add('show');
+        } else {
+            this._el.nativeElement.querySelector('.dropdown-menu').classList.remove('show');
+        }
+    }
 }
