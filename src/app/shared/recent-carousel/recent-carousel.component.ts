5c214064e25b8ba6f89b474148325f7d92471999
William Welling <wwelling@library.tamu.edu>
2019-08-30 09:53:00 -0500

    fixed carousel interval

--- src/app/shared/recent-carousel/recent-carousel.component.ts
+++ src/app/shared/recent-carousel/recent-carousel.component.ts
@@ -3,8 +3,8 @@ import { isPlatformServer, isPlatformBrowser } from '@angular/common';
 import { Component, ViewChild, ElementRef, AfterViewInit, HostListener, OnInit, Inject, PLATFORM_ID, OnDestroy } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
-import { BehaviorSubject, Observable, Subscription, timer } from 'rxjs';
-import { filter, take } from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subscription, interval } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 import { AppState } from '../../core/store';
 import { AppConfig } from '../../app.config';
@@ -40,7 +40,7 @@ export class RecentCarouselComponent implements AfterViewInit, OnInit, OnDestroy
 
     private limit = 20;
 
-    private delay = 10000;
+    private interval = 10000;
 
     @ViewChild('scrollView', { static: true })
     private scrollViewRef: ElementRef;
@@ -80,7 +80,7 @@ export class RecentCarouselComponent implements AfterViewInit, OnInit, OnDestroy
             }));
             this.fitItems();
             if (isPlatformBrowser(this.platformId)) {
-                this.subscriptions.push(timer(this.delay, this.delay).pipe(take(100)).subscribe(() => this.scrollRight()));
+                this.subscriptions.push(interval(this.interval).subscribe(() => this.scrollRight()));
             }
         }));
         this.store.dispatch(new fromSdr.RecentlyUpdatedResourcesAction(this.collection, {
