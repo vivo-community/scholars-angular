b8e172d420882e185a638a1fcfd584f43548c796
William Welling <wwelling@library.tamu.edu>
2020-04-09 10:48:30 -0500

    add formalize mapping for software type

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,6 @@ export const environment = {
   formalize: {
     otherUniversity: 'ExternalOrganization',
     GreyLiterature: 'InstitutionalRepositoryDocument',
-  },
+    ERO_0000071: 'Software'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,8 @@ export const environment = {
   formalize: {
     otherUniversity: 'ExternalOrganization',
     GreyLiterature: 'InstitutionalRepositoryDocument',
-  },
+    ERO_0000071: 'Software'
+  }
 };
 
 /*
