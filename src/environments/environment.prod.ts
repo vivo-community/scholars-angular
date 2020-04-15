dcc6de41b7313fc5e5d31afaebdf16f5f4981e36
Kevin Day <kday@library.tamu.edu>
2020-04-15 08:18:09 -0500

    Issue 9: Change type name: "Webpage" to "Internet Publication" in Scholars UI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
   formalize: {
     otherUniversity: 'ExternalOrganization',
     GreyLiterature: 'InstitutionalRepositoryDocument',
+    Webpage: 'InternetPublication',
     ERO_0000071: 'Software'
   }
 };
